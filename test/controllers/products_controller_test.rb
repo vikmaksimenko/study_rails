27ba7fae51d90876dadd74d4b7a2ca2c0cd935e5
vikmaksimenko <vikmaksimenko@hotmail.com>
2015-09-01 20:30:24 +0300

    study ruby units

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,13 +11,13 @@ class ProductsControllerTest < ActionController::TestCase
     }
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
